579913a6cf35c1e0d103d07a7ac5b0848f5f45d3
Thibault G <thibault.gerardin@gmail.com>
2024-02-23 11:58:59 +0100

    correction bug modale (suppression du defer sur le jquery et rajout de la partie modal de bootstrap.css)

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,7 +154,7 @@
         }
       });
       next =
-        imagesCollection[index-1] ||
+        imagesCollection[index] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,9 +192,7 @@
           index = i;
         }
       });
-      
-      next = imagesCollection[index+1] || imagesCollection[0];
-      
+      next = imagesCollection[index] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
